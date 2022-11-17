12cb118965e14a5b7be8d21c42447669fd8b778a
Felipe Costa <felipecosta8449@hotmail.com>
2022-11-17 16:36:07 -0300

    brownian motion interpolation.

--- brownian_motion.py
+++ brownian_motion.py
@@ -20,8 +20,30 @@ def BM_increment(dt, N, seed=1):
     return b
 
 
-# def brownian_motion_interpolation(bm_path):
-#     """Brownian motion through interpolation of a given path."""
-#     for i in range(len(bm_path)):
-#         n_mean = ()
-#         bs = np.random.normal(loc=)
+def brownian_motion_interpolation(bm_path, time_index, alpha=0.5):
+    """Brownian motion through interpolation of a given path.
+
+    alpha: parameter for the convex sum between s_0 and s_1 to get s.
+    """
+
+    interp_path = []
+    new_time_index = []
+
+    for s_0 in time_index[:-1]:
+
+        s_1 = time_index[time_index.index(s_0) + 1]
+        s = alpha * s_0 + (1 - alpha) * s_1
+        B_0 = bm_path[time_index.index(s_0)]
+        B_1 = bm_path[time_index.index(s_1)]
+
+        bs_mean = ((s_1 - s) * B_0 + (s - s_0) * B_1) / (s_1 - s_0)
+        bs_var = (s_1 - s) * (s - s_0) / (s_1 - s_0)
+
+        B_s = np.random.normal(loc=bs_mean, scale=bs_var, size=1)
+        interp_path.extend([B_0, B_s])
+        new_time_index.extend([s_0, s])
+
+    interp_path.append(bm_path[-1])
+    new_time_index.append(time_index[-1])
+
+    return interp_path, new_time_index
